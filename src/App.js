26da12c8fd2a344e5588e73cb31f5961cfec4af7
VivekPanchal <ivivekpanchal123@gmail.com>
2023-08-01 11:54:39 +0530

    up income done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useMemo} from 'react'
+import Orb from './Components/orb/Orb'
+import Navigation from './Components/navigation/Navigation'
+import { useGlobalContext } from './context/globalContext';
+import Dashboard from './Components/dashboard/Dashboard';
+import Income from './Components/income/Income'
+import Expenses from './Components/expenses/Expenses';
 
+import bg from './img/bg.png'
+import styled from "styled-components";
+import { MainLayout} from './styles/Layouts';
 function App() {
+  const [active, setActive] = useState(1)
+  const global = useGlobalContext()
+  const orbMemo = useMemo(() => {
+    return <Orb />
+  },[])
+  const displayData = () => {
+    switch(active){
+      case 1:
+        return <Dashboard />
+      case 2:
+        return <Dashboard />
+      case 3:
+        return <Income />
+      case 4: 
+        return <Expenses />
+      default: 
+        return <Dashboard />
+    }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AppStyled bg={bg} className='App'>
+      {orbMemo}
+      <MainLayout>
+      <Navigation active={active} setActive={setActive} />
+      <main>
+          {displayData()}
+        </main> 
+      </MainLayout>
+    </AppStyled>
   );
 }
-
+const AppStyled = styled.div`
+  height: 100vh;
+  background-image: url(${props => props.bg});
+  position: relative;
+  main{
+    flex: 1;
+    background: rgba(252, 246, 249, 0.78);
+    border: 3px solid #FFFFFF;
+    backdrop-filter: blur(4.5px);
+    border-radius: 32px;
+    overflow-x: hidden;
+    &::-webkit-scrollbar{
+      width: 0;
+    }
+  }
+`;
 export default App;
---
+++ src/context/globalContext.js
@@ -0,0 +1,49 @@
+import React, { useContext, useEffect, useState } from "react";
+import axios from "axios";
+
+const BASE_URL = "http://localhost:5000/api/v1/";
+
+const GlobalContext = React.createContext();
+
+export const GlobalProvider = ({ children }) => {
+  const [incomes, setIncomes] = useState([]);
+  const [expenses, setExpenses] = useState([]);
+  const [error, setError] = useState(null);
+  useEffect(() => {
+    getIncomes();
+  }, []);
+
+  const addIncome = async (income) => {
+    const response = await axios
+      .post(`${BASE_URL}add-income`, income)
+      .catch((err) => {
+        setError(err.response.data.message);
+      });
+  };
+  const deleteIncome = async (id) => {
+    const res  = await axios.delete(`${BASE_URL}delete-income/${id}`)
+    getIncomes()
+  }
+
+  const getIncomes = async () => {
+    const response = await axios.get(`${BASE_URL}get-incomes`);
+    setIncomes(response.data);
+    console.log(response.data);
+  };
+
+  return (
+    <GlobalContext.Provider
+      value={{
+        addIncome,
+        getIncomes,
+        incomes,
+        deleteIncome
+      }}
+    >
+      {children}
+    </GlobalContext.Provider>
+  );
+};
+export const useGlobalContext = () => {
+  return useContext(GlobalContext);
+};
